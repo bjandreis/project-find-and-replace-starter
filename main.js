277d262e0d0b1b2a60f4740e359ff92624ed299f
Brandon Andreis <bjandreis@gmail.com>
2020-06-08 14:14:11 -0400

    Finished assessment

--- main.js
+++ main.js
@@ -1,38 +1,46 @@
-// You should NOT change the HTML or CSS in this project (at least until you reach
-// the bonus objectives). Focus on the JavaScript.
-
 const findInput = document.querySelector(".find-input")
 const replaceInput = document.querySelector(".replace-input")
 const replaceAllButton = document.querySelector(".replace-all-button")
 
-// The following variable holds your OUTER ARRAY of row elements.
-// Later you will need an OUTER LOOP to loop over the individual elements within
-// this array.
 const rowElements = document.querySelectorAll(".row")
 
-// When you call the function belwo, it will get and return an INNER ARRAY
-// containing the cell elements for a given row.
-// Call this function from WITHIN your row elements loop. Then you will, in turn,
-// need to loop over the resulting cell elements. But where should this whole
-// NESTED LOOP go? Think through the user's experience: when should WHAT happen? 
 function getCellElements (currentRowElement) {
     return currentRowElement.querySelectorAll(".cell")
 }
 
+// 1. Add a click event listener to the replaceAllButton.
+replaceAllButton.addEventListener("click", function () {
+    // 2. On click but outside of a loop, assign the values of the two input textboxes ( findInput and replaceInput), so that you are only retrieving these values once per click.
+    const findField = findInput.value.trim()
+    const replaceWithField = replaceInput.value.trim()
 
-// YOUR CODE GOES HERE
+    console.log('find: ' + findField)
+    console.log('replace with: ' + replaceWithField)
 
+    // 3. Write a loop which loops over the rowElements array (which is already provided in the starter code).
+    for (rowIndex = 0; rowIndex < rowElements.length; rowIndex += 1) {
+        // 4. Inside this loop, use the getCellElements() function (already provided in the starter code) and assign the resulting array of cell elements to a variable.
+        const cellElements = getCellElements(rowElements[rowIndex])
 
-// One last thing: dedicate very careful attention to using variables and
-// naming them accurately.
-// And when you change the value you are assigning to a variable, don't
-// forget to consider changing the name to reflect the change you made! It
-// is very easy to get confused when you are working inside NESTED LOOPS.
-// The best of us do. And unnecessary confusion during the process of 
-// developing your code means wasted time.
-//
-// The time-cost of structuring and naming things well is FAR less than the
-// time-cost of ignoring the quality and readability of your code.
-//
-// You can, of course, remove any comments in this starter project once
-// you have read them, if you prefer.
+        console.log('---------------------------- NEW ROW ----------------------------')
+        let rowContent = rowElements[rowIndex].innerText
+        console.log(rowContent) // check contents of row
+
+        // 5. Write a nested loop which loops over the array of cell elements.
+        for (cellIndex = 0; cellIndex < cellElements.length; cellIndex += 1) {
+            console.log('---------------------------- NEW CELL ----------------------------')
+            let cellContent = cellElements[cellIndex].innerHTML
+            console.log(cellContent) // check contents of cell
+
+            // 6. For each cell element, check if a cell contains the user-provided search string. Use the string method includes().
+            if (cellContent.includes(findField)) {
+                console.log('"' + findField+ '"' + ' found in "' + cellContent + '"')
+                // 7. If a cell does contain the user-provided search string, use innerHTML and the string method replace() to replace the user-provided search string with the user-provided replacement string.
+                console.log('changing ' + '"' + cellContent + '" to ' + cellContent.replace(findField, replaceWithField))
+                cellElements[cellIndex].innerHTML = cellContent.replace(findField, replaceWithField)
+            } else {
+                console.log('"' + findField+ '"' + ' not found in "' + cellContent + '"')
+            }
+        }
+    }
+})
